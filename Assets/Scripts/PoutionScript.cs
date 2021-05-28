0ed48defd13a374fe4209ab7bd5bab35502afb77
Abdulkadir Karagishiev <kadyrkaragishiev@gmail.com>
2021-05-29 01:33:09 +0300

    Shit Commit

--- Assets/Scripts/PoutionScript.cs
+++ Assets/Scripts/PoutionScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 using Lean.Touch;
+using System;
 
 namespace LiquidVolumeFX
 {
@@ -12,34 +13,56 @@ namespace LiquidVolumeFX
         bool isTouch;
         public Text t;
         [SerializeField] Lunka currentLunka;
+        private LiquidVolume lv;
         public List<Lunka> collider_list;
-
+        Coroutine _fillenumerator;
         bool spilling;
-        private void LateUpdate()
-        {
-            float min_dist = Vector3.Distance(transform.position, collider_list[0].transform.position);
-            foreach(var lunka in collider_list)
-            {
-                if (Vector3.Distance(transform.position, lunka.transform.position)<min_dist && currentLunka !=lunka && !spilling)
-                {
-                    if (currentLunka != null) currentLunka._visible.SetActive(false);
-                    currentLunka = lunka;
-                    currentLunka._visible.SetActive(true);
-                }
-            }            
-        }
-        public void FillObject()
+
+
+
+        #region BeginTapAndEndTap
+        /// <summary>
+        //реализация работы наполнения сосуда жидкости через зажатие на кнопку
+        //максимальны гавнокод, как по мне, но работает не хуже швейцарских часов баля
+        /// </summary>
+        public void StartFillObject()
         {
             transform.DOMove(new Vector3(currentLunka.transform.position.x, transform.position.y, currentLunka.transform.position.z), 1f);
-            currentLunka._visible.SetActive(false);
-            StartCoroutine(fill());
+            #region oldcode
+            //currentLunka._visible.SetActive(false);
+            //StartCoroutine(fill());
+            #endregion
+            _fillenumerator = StartCoroutine(filling());
         }
-        
+        public void StopFilling()
+        {
+            StopCoroutine(_fillenumerator);
+        }
+        private IEnumerator filling()
+        {
+            while (true)
+            {
+                yield return new WaitForEndOfFrame();
+                lv.level += 0.005f;
+            }
+        }
+        #endregion
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.GetComponent<Lunka>() && !collider_list.Contains(other.gameObject.GetComponent<Lunka>()))
             {
                 collider_list.Add(other.gameObject.GetComponent<Lunka>());
+            }
+            float min_dist = Vector3.Distance(transform.position, collider_list[0].transform.position);
+            foreach (var lunka in collider_list)
+            {
+                if (Vector3.Distance(transform.position, lunka.transform.position) <= min_dist && currentLunka != lunka && !spilling)
+                {
+                    if (currentLunka != null) currentLunka._visible.SetActive(false);
+                    currentLunka = lunka;
+                    lv = currentLunka.GetComponentInChildren<LiquidVolume>();
+                    currentLunka._visible.SetActive(true);
+                }
             }
         }
         private void OnTriggerExit(Collider other)
@@ -57,23 +80,33 @@ namespace LiquidVolumeFX
         }
         private void OnTriggerStay(Collider other)
         {
-            t.text = "Уровень жидкости " + currentLunka.GetComponentInChildren<LiquidVolume>().level*100f + "%";
-        }
-        private IEnumerator fill()
-        {
-            spilling = true;
-            this.GetComponent<LeanDragTranslate>().enabled = false;
-            while (currentLunka.GetComponentInChildren<LiquidVolume>().level <0.9f)
+            try
             {
-                currentLunka.GetComponentInChildren<LiquidVolume>().level += 0.01F;
-                yield return new WaitForEndOfFrame();
+                if (currentLunka.gameObject != null)
+                    t.text = "Уровень жидкости " + currentLunka.GetComponentInChildren<LiquidVolume>().level * 100f + "%";
             }
-            yield return new WaitForSeconds(2f);
-            Destroy(currentLunka.gameObject.GetComponent<BoxCollider>());
-            currentLunka._visible.SetActive(false);
-            currentLunka = null;
-            this.GetComponent<LeanDragTranslate>().enabled = true;
-            spilling = false;
+            catch(Exception e)
+            {
+                Debug.Log("sorry");
+            }
+
         }
+        
+        //private IEnumerator fill()
+        //{
+        //    spilling = true;
+        //    this.GetComponent<LeanDragTranslate>().enabled = false;
+        //    while (currentLunka.GetComponentInChildren<LiquidVolume>().level <0.9f)
+        //    {
+        //        currentLunka.GetComponentInChildren<LiquidVolume>().level += 0.01F;
+        //        yield return new WaitForEndOfFrame();
+        //    }
+        //    yield return new WaitForSeconds(2f);
+        //    Destroy(currentLunka.gameObject.GetComponent<BoxCollider>());
+        //    currentLunka._visible.SetActive(false);
+        //    currentLunka = null;
+        //    this.GetComponent<LeanDragTranslate>().enabled = true;
+        //    spilling = false;
+        //}
     }
 }
